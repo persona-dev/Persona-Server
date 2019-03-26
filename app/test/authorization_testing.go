9f37a3f907fd726042248ac5cd5168188d1a4021
caffeine <eniehack@outlook.jp>
2019-03-26 14:07:35 +0900

    ログイン部分をDSLに記述した

--- app/test/authorization_testing.go
+++ app/test/authorization_testing.go
@@ -24,6 +24,78 @@ import (
 	"net/url"
 )
 
+// LoginAuthorizationOK runs the method Login of the given controller with the given parameters and payload.
+// It returns the response writer so it's possible to inspect the response headers.
+// If ctx is nil then context.Background() is used.
+// If service is nil then a default service is created.
+func LoginAuthorizationOK(t goatest.TInterface, ctx context.Context, service *goa.Service, ctrl app.AuthorizationController, payload *app.LoginAuthorizationPayload) http.ResponseWriter {
+	// Setup service
+	var (
+		logBuf bytes.Buffer
+
+		respSetter goatest.ResponseSetterFunc = func(r interface{}) {}
+	)
+	if service == nil {
+		service = goatest.Service(&logBuf, respSetter)
+	} else {
+		logger := log.New(&logBuf, "", log.Ltime)
+		service.WithLogger(goa.NewLogger(logger))
+		newEncoder := func(io.Writer) goa.Encoder { return respSetter }
+		service.Encoder = goa.NewHTTPEncoder() // Make sure the code ends up using this decoder
+		service.Encoder.Register(newEncoder, "*/*")
+	}
+
+	// Validate payload
+	err := payload.Validate()
+	if err != nil {
+		e, ok := err.(goa.ServiceError)
+		if !ok {
+			panic(err) // bug
+		}
+		t.Errorf("unexpected payload validation error: %+v", e)
+		return nil
+	}
+
+	// Setup request context
+	rw := httptest.NewRecorder()
+	u := &url.URL{
+		Path: fmt.Sprintf("/api/v1/auth/signature"),
+	}
+	req, _err := http.NewRequest("POST", u.String(), nil)
+	if _err != nil {
+		panic("invalid test " + _err.Error()) // bug
+	}
+	prms := url.Values{}
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	goaCtx := goa.NewContext(goa.WithAction(ctx, "AuthorizationTest"), rw, req, prms)
+	loginCtx, __err := app.NewLoginAuthorizationContext(goaCtx, req, service)
+	if __err != nil {
+		_e, _ok := __err.(goa.ServiceError)
+		if !_ok {
+			panic("invalid test data " + __err.Error()) // bug
+		}
+		t.Errorf("unexpected parameter validation error: %+v", _e)
+		return nil
+	}
+	loginCtx.Payload = payload
+
+	// Perform action
+	__err = ctrl.Login(loginCtx)
+
+	// Validate response
+	if __err != nil {
+		t.Fatalf("controller returned %+v, logs:\n%s", __err, logBuf.String())
+	}
+	if rw.Code != 200 {
+		t.Errorf("invalid response status code: got %+v, expected 200", rw.Code)
+	}
+
+	// Return results
+	return rw
+}
+
 // RegisterAuthorizationBadRequest runs the method Register of the given controller with the given parameters and payload.
 // It returns the response writer so it's possible to inspect the response headers and the media type struct written to the response.
 // If ctx is nil then context.Background() is used.
--- design/resources.go
+++ design/resources.go
@@ -14,8 +14,15 @@ var JWT = JWTSecurity("JWT", func() {
 
 var _ = Resource("Authorization", func() {
 	BasePath("/auth")
-	Security(JWT, func() {
-		Scope("api:access")
+	Action("login", func() {
+		Description("ログイン")
+		Routing(
+			POST("/signature"),
+		)
+		Payload(LoginPayload, func() {
+			Required("userid", "password")
+		})
+		Response(OK)
 	})
 	Action("register", func() {
 		Description("新規登録")
