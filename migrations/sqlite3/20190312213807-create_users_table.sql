a8bd7f7a6080cb31f0b9a317bc9b4961722c5b8e
caffeine <eniehack@outlook.jp>
2019-04-03 00:07:57 +0900

    Emailを追加する旨のマイグレーションを行った

--- migrations/sqlite3/20190312213807-create_users_table.sql
+++ migrations/sqlite3/20190312213807-create_users_table.sql
@@ -4,6 +4,7 @@
 CREATE TABLE users (
     user_id VARCHAR(15) NOT NULL PRIMARY KEY,
     screen_name VARCHAR(20) NOT NULL,
+    email VARCHAR(25) NOT NULL UNIQUE,
     delete_flag INT DEFAULT 0,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
