5e0880958a5c41c368b96f5d7d4a942a4e36af2c
caffeine <eniehack@outlook.jp>
2019-05-11 23:20:32 +0900

    PostgreSQLのBoolは小文字で指定するっぽいので修正

--- migrations/postgres/20190511203214-create_user_table.sql
+++ migrations/postgres/20190511203214-create_user_table.sql
@@ -5,11 +5,11 @@ CREATE TABLE users (
     user_id VARCHAR(15) NOT NULL PRIMARY KEY,
     screen_name VARCHAR(50) NOT NULL,
     email VARCHAR(254) NOT NULL UNIQUE,
-    delete_flag BOOLEAN DEFAULT 'False',
+    delete_flag BOOLEAN DEFAULT 'false',
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     password VARCHAR(150) NOT NULL,
-    is_admin BOOLEAN DEFAULT 'False'
+    is_admin BOOLEAN DEFAULT 'false'
 );
 
 -- +migrate Down
--- migrations/postgres/20190511215942-create_post_table.sql
+++ migrations/postgres/20190511215942-create_post_table.sql
@@ -4,7 +4,7 @@
 CREATE TABLE posts (
     post_id VARCHAR(26) NOT NULL PRIMARY KEY,
     user_id VARCHAR(15) NOT NULL,
-    delete_flag BOOLEAN DEFAULT 0 NOT NULL,
+    delete_flag BOOLEAN DEFAULT 'false' NOT NULL,
     body VARCHAR(500) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
