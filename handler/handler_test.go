9562dd3d7a53dbe221e394d91fe7ba2a1a8583be
caffeine <eniehack@outlook.jp>
2019-05-19 15:55:39 +0900

    assertを使わずテストを設計した

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,19 +1,30 @@
 package handler
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	migrate "github.com/rubenv/sql-migrate"
-	"github.com/stretchr/testify/assert"
+	"gopkg.in/go-playground/validator.v9"
 
 	"github.com/labstack/echo"
 )
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
 
 /*
 func TestMain(m *testing.M) {
@@ -43,11 +54,15 @@ func SetUpDataBase() (*Handler, error) {
 }
 
 func TestRegister(t *testing.T) {
-	UserID := "testuser"
-	RequestJSON := `{"user_id":"testuser","email":"testuser@example.com","screen_name":"testuser1","password":"password"}`
-	RegisteredResponse := fmt.Sprintf(`{"status_code": "201", "account_url": "/users/%s"}`, UserID)
+	type ResponseParams struct {
+		URL    string `json:"account_url"`
+		Status string `json:"status_code"`
+	}
+	Response := new(ResponseParams)
+	RequestJSON := `{"userid":"testuser","email":"testuser@example.com","screen_name":"testuser1","password":"password"}`
 
 	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
 	req := httptest.NewRequest(http.MethodPost, "/api/v1/auth/new", strings.NewReader(RequestJSON))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
@@ -57,31 +72,90 @@ func TestRegister(t *testing.T) {
 	if err != nil {
 		fmt.Println(err)
 	}
+	defer h.DB.Close()
 
-	if assert.NoError(t, h.Register(c)) {
-		assert.Equal(t, http.StatusCreated, rec.Code)
-		assert.Equal(t, RegisteredResponse, rec.Body.String())
+	if err := h.Register(c); err != nil {
+		t.Fatalf("failed: Register(): %s", err)
+	}
+
+	if rec.Code != http.StatusCreated {
+		t.Fatalf("failed: Register() responsed different status code: %d", rec.Code)
+	}
+
+	if err := json.Unmarshal(rec.Body.Bytes(), &Response); err != nil {
+		t.Fatalf("failed: json.Unmarshal(): %s", err)
+	}
+
+	if Response.Status != "201" && Response.URL != "/users/testuser/" {
+		t.Fatalf("failed: Register() responsed different body: %s", rec.Body)
 	}
 }
 
 func TestLogin(t *testing.T) {
-	UserID := "testuser"
-	RequestJSON := `{"user_id":"testuser","password":"password"}`
-	RegisteredResponse := fmt.Sprintf(`{"status_code": "201", "account_url": "/users/%s"}`, UserID)
+	type ResponseParams struct {
+		Token string `json:"token"`
+	}
+	type TokenPayload struct {
+		ExpiresAt int64  `json:"exp"`
+		IssuedAt  int64  `json:"iat"`
+		NotBefore int64  `json:"nbf"`
+		Audience  string `json:"aud"`
+	}
+	Response := new(ResponseParams)
+	RequestJSON := `{"userid":"testuser","password":"password"}`
 
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/api/v1/auth/new", strings.NewReader(RequestJSON))
+	e.Validator = &CustomValidator{validator: validator.New()}
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(RequestJSON))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
+	c.SetPath("api/v1/auth/signature")
 
 	h, err := SetUpDataBase()
 	if err != nil {
 		fmt.Println(err)
 	}
+	defer h.DB.Close()
 
-	if assert.NoError(t, h.Register(c)) {
-		assert.Equal(t, http.StatusCreated, rec.Code)
-		assert.Equal(t, RegisteredResponse, rec.Body.String())
+	if err := h.Login(c); err != nil {
+		t.Fatalf("failed: Login(): %s", err)
+	}
+
+	if rec.Code != http.StatusOK {
+		t.Fatalf("failed: Login() responsed different status code: %d", rec.Code)
+	}
+
+	if err := json.Unmarshal(rec.Body.Bytes(), &Response); err != nil {
+		t.Fatalf("failed: json.Unmarshal(): %s", err)
+	}
+
+	fmt.Println(Response.Token)
+
+	Token := strings.Split(Response.Token, ".")
+	Header := `{"alg":"RS512","typ":"JWT"}`
+	ResponseHeader, err := base64.URLEncoding.DecodeString(Token[0])
+	if err != nil {
+		t.Fatalf("failed: base64.URLEncoding.DecodeString(): %s", err)
+	}
+	if string(ResponseHeader[:]) != Header {
+		t.Fatalf("failed: Header is %s", ResponseHeader)
+	}
+
+	ResponsePayload, err := base64.URLEncoding.DecodeString(Token[1])
+	if err != nil {
+		t.Fatalf("failed: base64.URLEncoding.DecodeString(): %s", err)
+	}
+
+	Payload := new(TokenPayload)
+	if err := json.Unmarshal(ResponsePayload, &Payload); err != nil {
+		t.Fatalf("failed: json.Unmarshal(): %s", err)
+	}
+
+	if Payload.Audience != "testuser" {
+		t.Fatalf("failed: Payload is %s", ResponsePayload)
+	}
+	if Payload.ExpiresAt < time.Now().Add(time.Minute*5).Unix() {
+		t.Fatalf("failed: jwt token's exp invaild")
 	}
 }
